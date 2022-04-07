30c55d22617ad4b954e06b1abe5368cca6c942d6
Aron Lin <aronl9810@hstat.org>
2022-04-07 15:22:29 +0000

    (FIX)navbar (CHANGE)Added Offcanvas

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-h1 {
-    color: red;
-}
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
